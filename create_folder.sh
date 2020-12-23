e0f9a1f57981cfcd6cc2830803e8c869965a7210
csqray <csqray@126.com>
2020-12-23 02:43:10 +0000

    third commit

---
+++ create_folder.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir train_set
+mkdir test_set
+mkdir pre_trained
+mkdir pre_trained/gene
+mkdir pre_trained/model
+mkdir pre_trained/metrics_and_labels
+mkdir pre_trained/metrics_and_labels/new_model
