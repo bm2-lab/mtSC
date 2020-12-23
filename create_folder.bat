1f26b49279da2e81211cb11f8b239aa14375cac1
csqray <54893226+csqray@users.noreply.github.com>
2020-12-23 14:15:57 +0800

    Update create_folder.bat

--- create_folder.bat
+++ create_folder.bat
@@ -2,7 +2,7 @@
 mkdir train_set
 mkdir test_set
 mkdir pre_trained
-mkdir pre_trained/gene
-mkdir pre_trained/model
-mkdir pre_trained/metrics_and_labels
-mkdir pre_trained/metrics_and_labels/new_model
+mkdir pre_trained\gene
+mkdir pre_trained\model
+mkdir pre_trained\metrics_and_labels
+mkdir pre_trained\metrics_and_labels\new_model
